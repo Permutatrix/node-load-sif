c8cbc336930376c6dbed29d1c20bd7bd057429d5
Permutator <permutatorem@gmail.com>
2015-10-06 21:40:17 -0700

    Set up gulp and stuff

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var del = require('del');
+
+
+gulp.task('clean', function(cb) {
+  del('lib/**/*').then(function() { cb(); });
+});
+
+gulp.task('default', ['clean'], function() {
+  return gulp.src('src/**/*.js')
+  .pipe(babel())
+  .pipe(gulp.dest('lib'));
+});
