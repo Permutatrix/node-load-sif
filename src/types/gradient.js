8f988a5f6d948862346ffabaa66dfbf6d2a253d7
Permutator <permutatorem@gmail.com>
2015-12-30 17:17:15 -0800

    Made Gradient and Matrix not bare arrays anymore

--- src/types/gradient.js
+++ src/types/gradient.js
@@ -4,24 +4,24 @@ import { insertSorted } from '../utils.js';
 
 
 export function threeColor(c1, c2, c3) {
-  return [ Stop.create(0, c1), Stop.create(0.5, c2), Stop.create(1, c3) ];
+  return { stops: [ Stop.create(0, c1), Stop.create(0.5, c2), Stop.create(1, c3) ] };
 }
 
 export function twoColor(c1, c2) {
-  return [ Stop.create(0, c1), Stop.create(1, c2) ];
+  return { stops: [ Stop.create(0, c1), Stop.create(1, c2) ] };
 }
 
 export function oneColor(color) {
-  return [ Stop.create(0, color) ];
+  return { stops: [ Stop.create(0, color) ] };
 }
 
 export function empty() {
-  return [];
+  return { stops: [] };
 }
 
 
 export function addStop(gradient, stop) {
-  insertSorted(gradient, stop, Stop.comesAfter);
+  insertSorted(gradient.stops, stop, Stop.comesAfter);
 }
 
 export function addNewStop(gradient, position, color) {
--- src/types/matrix.js
+++ src/types/matrix.js
@@ -1,27 +1,35 @@
 export function create(m00, m01, m02,
-                                      m10, m11, m12,
-                                      m20, m21, m22) {
-  return [m00, m01, m02,
-          m10, m11, m12,
-          m20, m21, m22];
+                       m10, m11, m12,
+                       m20, m21, m22) {
+  return {
+    array: [m00, m01, m02,
+            m10, m11, m12,
+            m20, m21, m22]
+  };
 }
 
 export function affine(m00, m01,
                        m10, m11,
                        m20, m21) {
-  return [m00, m01, 0,
-          m10, m11, 0,
-          m20, m21, 1];
+  return {
+    array: [m00, m01, 0,
+            m10, m11, 0,
+            m20, m21, 1]
+  };
 }
 
 export function fromVectors(axisX, axisY, offset) {
-  return [axisX.x,  axisX.y,  0,
-          axisY.x,  axisY.y,  0,
-          offset.x, offset.y, 1];
+  return {
+    array: [axisX.x,  axisX.y,  0,
+            axisY.x,  axisY.y,  0,
+            offset.x, offset.y, 1]
+  };
 }
 
 export function identity() {
-  return [1, 0, 0,
-          0, 1, 0,
-          0, 0, 1];
+  return {
+    array: [1, 0, 0,
+            0, 1, 0,
+            0, 0, 1]
+  };
 }
